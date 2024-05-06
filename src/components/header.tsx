807760e390be4ac8cc794b7ec48611ad6f20fe24
Webster <webster.rafael2014@gmail.com>
2024-05-06 14:06:42 -0400

    Att

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import { NavLink } from './nav-link';
 export function Header() {
   return (
     <div className="flex items-center gap-5 py-2">
-      <img className='size-8 rounded-lg' src='src/assets/webb-logo.svg' alt="Webb Icon" />
+      <img className='size-8 rounded-lg' src='/webb-logo.svg' alt="Webb Icon" />
 
       <nav className="flex items-center gap-5">
         <NavLink classe={"opacity-60"} href="/eventos">Eventos</NavLink>
